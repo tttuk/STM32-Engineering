64ed4330868289c200bec35f08b154c3826cd355
Shylock <thk20071103@fixmail.com>
2026-07-15 16:06:57 +0800

    新增实验3(IR红外发送)、实验4(IR红外接收)、实验5(I2C与E2PROM)

---
+++ 实验3_IR红外发送实验/BSP/usart.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include "usart.h"
+
+void UART2_Init(u32 bound)
+{
+    GPIO_InitTypeDef GPIO_InitStructure;
+    USART_InitTypeDef USART_InitStructure;
+
+    //使能GPIOA和USART2时钟
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA,ENABLE);  //GPIOA在AHB1高速总线上
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2,ENABLE); //USART2在APB1硬件总线上
+
+    //USART2 对应引脚复用映射
+    GPIO_PinAFConfig(GPIOA, GPIO_PinSource2, GPIO_AF_USART2);
+    GPIO_PinAFConfig(GPIOA, GPIO_PinSource3, GPIO_AF_USART2);
+
+    //USART2 IO配置
+    GPIO_InitStructure.GPIO_Pin  = GPIO_Pin_2 | GPIO_Pin_3;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;     //复用
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;//速度50MHz
+    GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;   //推挽复用输出
+    GPIO_InitStructure.GPIO_PuPd  = GPIO_PuPd_UP;
+    GPIO_Init(GPIOA,&GPIO_InitStructure);
+
+    //USART2 端口配置
+    USART_InitStructure.USART_BaudRate = bound;  //波特率设置
+    USART_InitStructure.USART_WordLength = USART_WordLength_8b;  //字长8bit
+    USART_InitStructure.USART_StopBits   = USART_StopBits_1;       //1停止位
+    USART_InitStructure.USART_Parity     = USART_Parity_No;          //无校验
+    USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;//无硬件数据流控制
+    USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;    //收发
+    USART_Init(USART2, &USART_InitStructure);
+
+    USART_Cmd(USART2, ENABLE);  //使能串口2
+}
+
+//重定向c库函数printf到串口，重定向后可使用printf函数
+int fputc(int ch, FILE *f)
+{
+    // 发送一个字节数据到串口
+    USART_SendData(USART2, (uint8_t) ch);
+
+    // 等待发送完毕
+    while (USART_GetFlagStatus(USART2, USART_FLAG_TXE) == RESET);
+
+    return (ch);
+}
---
+++ 实验3_IR红外发送实验/UTILS/delay.c
@@ -0,0 +1,34 @@
+#include "delay.h"
+
+void systick_delay_us(uint32_t us){
+    uint32_t ticks = 0;
+    uint32_t told,tnow,reload,tcnt = 0;
+
+    reload = SysTick->LOAD;           //获取重装载寄存器值
+    ticks  = us * (SystemCoreClock / 1000000);  //需要的总计数值
+    told   = SysTick->VAL;                   //获取当前数值寄存器值（开始时数值）
+
+    while( 1 ){
+        tnow = SysTick->VAL;
+        if(tnow == told){//没有差值不需处理
+          continue ;
+        }
+
+        if(tnow < told){             //当前值小于开始数值，说明未计到0
+          tcnt += told - tnow;
+        }
+        else {//当前值大于开始数值，说明已计到0并重新计数
+          tcnt += reload - tnow + told;
+        }
+
+        told = tnow;
+        if(tcnt >= ticks) { break; }
+    }
+}
+
+void delay_ms(uint32_t ms){
+    uint32_t i, j;
+    for(i = 0; i < ms; i++){
+        for(j = 0; j < 21000; j++);  //约1ms延时 @168MHz
+    }
+}
